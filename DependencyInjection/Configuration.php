20708948ba0a3c945875c5ab472f489b9e5e33f6
Pierre Hennequart <pierre@janalis.com>
2021-06-22 03:50:12 +0200

    fix configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,8 +12,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('naoned_oai_pmh_server');
+        $treeBuilder = new TreeBuilder('naoned_oai_pmh_server');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
